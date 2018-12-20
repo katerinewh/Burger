c5dcf691c134ffe07581ea2268e47ddae5fca15b
Katerine <katerinehw@att.net>
2018-12-19 17:07:42 -0800

    framework and code insertion

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger on a Brioche Bun');
+INSERT INTO burgers (burger_name) VALUES ('Bo Burger (cheeseburger with fried egg');
+INSERT INTO burgers (burger_name) VALUES ('Double Animal Style, Protein Style');
+INSERT INTO burgers (burger_name) VALUES ('Dyer’s Deep-Fried Burger');
+INSERT INTO burgers (burger_name) VALUES ('Kobe Burger');
